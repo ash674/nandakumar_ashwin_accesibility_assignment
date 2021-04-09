18c128c48c782a964a7fcb612f484bdf8d2531fc
ashwin <ashu61998@gmail.com>
2021-04-10 02:09:32 +0530

    Added Js php scss

---
+++ js/components/DisplayComp.js
@@ -0,0 +1,31 @@
+import { fetchData } from "./DataMiner.js";
+import MiniFile from "./images.js";
+
+export default {
+  name: "Images",
+  components: {
+    MiniFile,
+  },
+
+  data: function () {
+    return {
+      files: [],
+    };
+  },
+
+  mounted: function () {
+    fetchData("./includes/index.php")
+      .then((data) => (this.files = data))
+      .catch((err) => console.error(err));
+  },
+
+  template: `
+    <div class="container-fluid">
+      <div v-if="files.length > 0" class="row">
+        <div class="col-md-4" v-for="f in files" :key="f.id">
+          <mini-file :file="f"></mini-file>
+        </div>
+      </div>
+    </div>
+  `,
+};
---
+++ js/components/Video.js
@@ -0,0 +1,31 @@
+import { fetchData } from "./DataMiner.js";
+
+export default {
+  name: "TheVidDetail",
+
+  data: function () {
+    return {
+      id: this.$route.params.id,
+      file: null
+    };
+  },
+
+  template: `
+    <div>
+    <section v-if="file">
+      <video controls class="video" ref="video" :src="'./images/' + file.video" type="video/mp4">
+      </video>
+  <p>Playing Now: {{ file.descrip }} <p>
+    
+
+      </section>
+    
+ </div>
+  `,
+
+  mounted: function () {
+    fetchData("./includes/index.php?id=" + this.id)
+      .then((data) => (this.file = data[0]))
+      .catch((err) => console.error(err));
+  }
+};
